d6fb73ee81e69f490b5c669aacc35f708d5724e2
Polina Chertsova <Polina.Chertsova@sidenis.com>
2018-12-21 10:39:03 +0700

    Refactoring

--- src/test/java/user/system/Base.java
+++ src/test/java/user/system/Base.java
@@ -2,12 +2,9 @@ package user.system;
 
 import com.github.javafaker.Faker;
 import org.junit.Before;
-import org.junit.Test;
 
-import static io.restassured.module.jsv.JsonSchemaValidator.matchesJsonSchemaInClasspath;
 import static user.system.config.Config.*;
 import static user.system.setups.RestSetups.*;
-import static user.system.utils.Utils.*;
 
 
 public class Base {
@@ -17,32 +14,6 @@ public class Base {
     public void before() {
         setBaseURI(BASE_URI);
         setBasePath(BASE_PATH);
-    }
-
-    @Test
-    public void shouldRespondWithOkAndMatchSchema() {
-        String requestBody = getResourceAsString("request-bodies/required-fields");
-        requestBody = String.format(requestBody, getRandomEmail(), getRandomName());
-
-        createUserWithTask(requestBody)
-        .then()
-                .statusCode(200)
-                .assertThat()
-                .body(matchesJsonSchemaInClasspath("json-schemas/required-fields"));
-    }
-
-    @Test
-    public void shouldRespondWithOkAndMatchSchemaForAllFields() {
-        String requestBody = getResourceAsString("request-bodies/all-fields");
-        requestBody = String.format(requestBody, getRandomEmail(), getRandomName());
-
-        createUserWithTask(requestBody)
-                .then()
-                .log()
-                .all()
-                .statusCode(200)
-                .assertThat()
-                .body(matchesJsonSchemaInClasspath("json-schemas/all-fields"));
     }
 
     protected String getRandomEmail() {
---
+++ src/test/java/user/system/CheckSchemas.java
@@ -0,0 +1,36 @@
+package user.system;
+
+import org.junit.Test;
+
+import static io.restassured.module.jsv.JsonSchemaValidator.matchesJsonSchemaInClasspath;
+import static user.system.setups.RestSetups.createUserWithTask;
+import static user.system.utils.Utils.getResourceAsString;
+
+
+public class CheckSchemas extends Base{
+    @Test
+    public void shouldRespondWithOkAndMatchSchema() {
+        String requestBody = getResourceAsString("request-bodies/required-fields");
+        requestBody = String.format(requestBody, getRandomEmail(), getRandomName());
+
+        createUserWithTask(requestBody)
+                .then()
+                .statusCode(200)
+                .assertThat()
+                .body(matchesJsonSchemaInClasspath("json-schemas/required-fields"));
+    }
+
+    @Test
+    public void shouldRespondWithOkAndMatchSchemaForAllFields() {
+        String requestBody = getResourceAsString("request-bodies/all-fields");
+        requestBody = String.format(requestBody, getRandomEmail(), getRandomName());
+
+        createUserWithTask(requestBody)
+                .then()
+                .log()
+                .all()
+                .statusCode(200)
+                .assertThat()
+                .body(matchesJsonSchemaInClasspath("json-schemas/all-fields"));
+    }
+}
