16c62fe2a4539d86e4713cecc93eb7ee9a1e85e2
Polina Chertsova <Polina.Chertsova@sidenis.com>
2018-12-24 17:49:33 +0700

    Add negative tests for email checking

--- src/test/java/user/system/CheckEmail.java
+++ src/test/java/user/system/CheckEmail.java
@@ -12,8 +12,93 @@ public class CheckEmail extends Base {
     private static final String PATH_TO_JSON = "request-bodies/email/";
 
     @Test
-    public void shouldRespondWithErrorForInvalidEmail() {
-        String requestBody = getResourceAsString(PATH_TO_JSON +"invalid");
+    public void shouldRespondWithErrorForEmailWithDoubleAtSign() {
+        String requestBody = getResourceAsString(PATH_TO_JSON + "invalid/double-at-sign");
+
+
+        createUserWithTask(requestBody)
+                .then()
+                .statusCode(200)
+                .body("type", equalTo("error"))
+                .body("message", equalTo("email неправильный!"));
+    }
+
+    @Test
+    public void shouldRespondWithErrorForEmailWithLeadingDot() {
+        String requestBody = getResourceAsString(PATH_TO_JSON + "invalid/leading-dot");
+
+
+        createUserWithTask(requestBody)
+                .then()
+                .statusCode(200)
+                .body("type", equalTo("error"))
+                .body("message", equalTo("email неправильный!"));
+    }
+
+    @Test
+    public void shouldRespondWithErrorForEmailWithLongLength() {
+        String requestBody = getResourceAsString(PATH_TO_JSON + "invalid/long-length");
+
+
+        createUserWithTask(requestBody)
+                .then()
+                .statusCode(200)
+                .body("type", equalTo("error"))
+                .body("message", equalTo("email неправильный!"));
+    }
+
+    @Test
+    public void shouldRespondWithErrorForEmailWithUnicodeChars() {
+        String requestBody = getResourceAsString(PATH_TO_JSON + "invalid/unicode-chars");
+
+
+        createUserWithTask(requestBody)
+                .then()
+                .statusCode(200)
+                .body("type", equalTo("error"))
+                .body("message", equalTo("email неправильный!"));
+    }
+
+    @Test
+    public void shouldRespondWithErrorForEmailWithoutAtSign() {
+        String requestBody = getResourceAsString(PATH_TO_JSON + "invalid/without-at-sign");
+
+
+        createUserWithTask(requestBody)
+                .then()
+                .statusCode(200)
+                .body("type", equalTo("error"))
+                .body("message", equalTo("email неправильный!"));
+    }
+
+    @Test
+    public void shouldRespondWithErrorForEmailWithoutDot() {
+        String requestBody = getResourceAsString(PATH_TO_JSON + "invalid/without-dot");
+
+
+        createUserWithTask(requestBody)
+                .then()
+                .statusCode(200)
+                .body("type", equalTo("error"))
+                .body("message", equalTo("email неправильный!"));
+    }
+
+    @Test
+    public void shouldRespondWithErrorForEmailWithoutTopLevelDomain() {
+        String requestBody = getResourceAsString(PATH_TO_JSON + "invalid/without-top-level-domain");
+
+
+        createUserWithTask(requestBody)
+                .then()
+                .statusCode(200)
+                .body("type", equalTo("error"))
+                .body("message", equalTo("email неправильный!"));
+    }
+
+    @Test
+    public void shouldRespondWithErrorForEmailWithoutUsername() {
+        String requestBody = getResourceAsString(PATH_TO_JSON + "invalid/without-username");
+
 
         createUserWithTask(requestBody)
                 .then()
@@ -43,7 +128,7 @@ public class CheckEmail extends Base {
 
     @Test
     public void shouldRespondWithErrorForRequestWithoutEmail() {
-        String requestBody = Utils.getResourceAsString(PATH_TO_JSON +"without");
+        String requestBody = Utils.getResourceAsString(PATH_TO_JSON + "without");
 
         createUserWithTask(requestBody)
                 .then()
